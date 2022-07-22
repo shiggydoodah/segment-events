5077afe79b13235273186028c7c8894a6833ae98
Louis Lombardi <louis@louislombardi.co.uk>
2022-07-22 11:10:03 +0100

    update package

--- lib/cjs/index.d.ts
+++
@@ -1,4 +0,0 @@
-import * as trackEvents from './track';
-declare const initSegment: (segmentKey: string) => void;
-declare const track: typeof trackEvents;
-export { initSegment, track };
--- lib/cjs/segment.d.ts
+++
@@ -1,6 +0,0 @@
-declare global {
-    interface Window {
-        analytics?: any;
-    }
-}
-export default function (segmentKey: string): void;
